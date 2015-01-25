6a3699d8c7bdfd04798366f91ca1ab964606ae58
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2015-01-25 01:34:00 +0000

    First implementation

---
+++ di.js
@@ -0,0 +1,68 @@
+var assert = require('assert');
+var format = require('util').format;
+
+var Di = function (/*values*/) {
+	this._definitions = {};
+	this._factory = [];
+};
+
+Di.prototype = {
+	_has: function (id) {
+		return typeof this._definitions[id] === 'undefined' ? false : true;
+	},
+	_single: function (func) {
+		return function (di) {
+			this.value = func(di);
+			return this.value;
+		};
+	},
+	set: function (id, funcOrValue) {
+		assert(arguments.length >= 2, 'Two arguments required');
+		assert(typeof id === 'string', 'Expected string id');
+		assert.equal(this._has(id), false, format('Identifier "%s" already defined', id));
+
+		var isFunction = typeof funcOrValue === 'function',
+			isInFactory = isFunction && this._factory.indexOf(funcOrValue) !== -1;
+
+		this._definitions[id] = isFunction
+			? { func: isInFactory ? funcOrValue : this._single(funcOrValue) }
+			: { value: funcOrValue };
+		this._factory = [];
+
+		return this;
+	},
+	get: function (id) {
+		assert(arguments.length >= 1, 'One argument required');
+		assert(typeof id === 'string', 'Expected string id');
+		assert.equal(this._has(id), true, format('Identifier "%s" is not defined', id));
+
+		var definition = this._definitions[id],
+			hasValue = Object.keys(definition).indexOf('value') !== -1;
+
+		return hasValue
+			? definition.value
+			: definition.func(this);
+	},
+	factory: function (func) {
+		assert(arguments.length >= 1, 'One argument required');
+		assert(typeof func === 'function', 'Expected function func');
+		this._factory.push(func);
+
+		return func;
+	},
+	/*protect: function (func) {
+	},
+	raw: function (id) {
+	},
+	extend: function (id, func) {
+	},
+	has: function (id) {
+	},
+	remove: function ($id) {
+	},
+	keys: function () {
+	}
+	*/
+};
+
+module.exports = Di;
