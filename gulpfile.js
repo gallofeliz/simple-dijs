093b77d6ac8b1dad74707d0bf3015f914636a017
Aubin VIGHIER <avighier@users.noreply.github.com>
2016-07-03 03:39:40 -0500

    Fixed ESLint violations

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 gulp.task('lint', function () {
-	return gulp.src(['src/*.js', '*.js', 'test/*.js'])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
+    return gulp.src(['src/*.js', '*.js', 'test/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
