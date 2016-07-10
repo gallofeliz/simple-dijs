7368f6f5528934bc8be06821eb49be7734e1cc08
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2016-07-10 00:08:06 -0500

    Consolidation for previous Gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
 
+gulp.task('default', ['build']);
+gulp.task('build', ['checks', 'build-dist', 'build-minify']);
+gulp.task('checks', ['lint', 'test', 'browser-test', 'test-npm-package']);
+
 gulp.task('lint', function () {
     return gulp.src(['src/*.js', '*.js', 'test/*.js'])
         .pipe(eslint())
@@ -17,13 +21,7 @@ gulp.task('lint', function () {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('clean', function () {
-    return del([
-        'coverage'
-    ]);
-});
-
-gulp.task('build-dist', function () {
+gulp.task('build-dist', ['lint'], function () {
     mkdirp.sync('dist');
 
     return browserify(
@@ -42,8 +40,9 @@ gulp.task('build-minify', ['build-dist'], function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('test', ['lint', 'clean'], function (cb) {
-    return gulp.src(['dist/*.js'])
+gulp.task('test', ['lint', 'build-dist'], function (cb) {
+    del.sync(['coverage']);
+    return gulp.src(['dist/di.js'])
                 .pipe(istanbul({includeUntested: true}))
                 .pipe(istanbul.hookRequire())
                 .on('end', function () {
@@ -58,7 +57,7 @@ gulp.task('test', ['lint', 'clean'], function (cb) {
                 });
 });
 
-gulp.task('browser-test', function (cb) {
+gulp.task('browser-test', ['build-minify'], function (cb) {
 
     browserify(
         ['test/di.js']
@@ -78,10 +77,11 @@ gulp.task('browser-test', function (cb) {
 
 });
 
-gulp.task('test-npm-package', function (cb) {
+gulp.task('test-npm-package', ['build-dist', 'build-minify'], function (cb) {
 
     var exec = require('child_process').exec,
         mainFile = require('./package.json').main,
+        miniFile = mainFile.replace(/\.js$/, '.min.js'),
         fs = require('fs'),
         path = require('path'),
         dir = fs.mkdtempSync(process.env.TEMP + path.sep + 'tnp-'),
@@ -92,21 +92,26 @@ gulp.task('test-npm-package', function (cb) {
 
     exec('npm install --ignore-scripts ' + source, {cwd: dir}, function (error, stdout, stderr) {
 
-        console.log(stdout);
-        console.error(stderr);
-
         if (error) {
             finish(error);
             return;
         }
 
-        fs.access(path.join(dir, 'node_modules', 'simple-dijs', mainFile), function (e) {
-            if (!e) {
-                console.log(mainFile + ' checked');
+        var missingFiles = [ mainFile, miniFile ].filter(function (filename) {
+            try {
+                fs.accessSync(path.join(dir, 'node_modules', 'simple-dijs', filename));
+                return false;
+            } catch (e) {
+                return true;
             }
-
-            finish(e);
         });
 
+        if (missingFiles.length > 0) {
+            finish('Missing files ' + missingFiles.join(', '));
+            return;
+        }
+
+        console.log('No missing files');
+        finish();
     });
 });
